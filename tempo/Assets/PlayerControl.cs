6fb1795f6877b717a14b51761fdfa48f5ddaf6b6
absou4000121 <39017409+absou4000121@users.noreply.github.com>
2018-05-06 12:09:03 +0900

    hankura

---
+++ tempo/Assets/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    Vector2 vec;
+    public float speed = 2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Debug.Log("LeftClick:" + mousePosition);
+
+            vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(vec.x, vec.y), speed * Time.deltaTime);
+
+    }
+}
